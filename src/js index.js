b2034b4bc299fdb47c41ffcc51a1c1c7a022f6df
neo050 <neoray.asking666@gmail.com>
2023-03-25 21:20:00 +0300

    new

---
+++ src/js index.js
@@ -0,0 +1,18 @@
+import  express  from "express";
+const port =process.env.port||3000;
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    console.log('A new request has arrived to index.js');
+    res.send("Hello from the server main page");
+
+}
+);
+app.listen(port,()=>{
+    console.log('Server is up and runnig at port : '+port);
+}
+);
+
+
+
